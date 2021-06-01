2317587dfe6fdfd24af31a46ce1fe203c793a22d
Rey <reyfundora@icloud.com>
2021-05-31 21:54:36 -0400

    fixed spacing.

--- src/main/java/ex01.java
+++ src/main/java/ex01.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ex01 {
     public static void main (String[] args){
-        System.out.println("What is your name?");
+        System.out.print("What is your name? ");
         Scanner input = new Scanner(System.in);
         String name = input.next();
 
